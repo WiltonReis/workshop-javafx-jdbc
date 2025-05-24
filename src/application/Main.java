2d279e805c32fb7ac022ed3c3b3d9150c6180b8c
Wilton Reis <wiltonfilho0825@gmail.com>
2025-05-24 12:34:37 -0300

    Implementacao tela 'Sobre'

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,18 +8,26 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private static Scene mainScene;
+
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
         ScrollPane scrollPane = loader.load();
-        Scene scene = new Scene(scrollPane);
+        mainScene = new Scene(scrollPane);
         scrollPane.setFitToHeight(true);
         scrollPane.setFitToWidth(true);
-        stage.setScene(scene);
+        stage.setScene(mainScene);
         stage.setTitle("Workshop");
         stage.show();
     }
+
+    public static Scene getMainScene(){
+        return mainScene;
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
+
 }
